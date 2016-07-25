fd9149bca264e303b6293efb0770872f867ff7e7
Larry Smith Jr <mrlesmithjr@gmail.com>
2016-07-25 15:09:46 -0400

    Updated Admin script
    Signed-off-by: Larry Smith Jr <mrlesmithjr@gmail.com>

--- Vagrant/admin-openrc.sh
+++ Vagrant/admin-openrc.sh
@@ -6,4 +6,3 @@ export OS_USERNAME=admin
 export OS_PASSWORD=29b1416692cb38014ea0
 export OS_AUTH_URL=http://openstack.vagrant.local:35357/v2.0
 export OS_VOLUME_API_VERSION=2
-export OS_IMAGE_API_VERSION=2
